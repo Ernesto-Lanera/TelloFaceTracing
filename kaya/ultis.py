0ad33c1b63007c246c78166e72eb67dbd7af59c2
kayajb <kayabrasser@gmail.com>
2023-09-27 14:24:37 +0200

    test 5

--- kaya/ultis.py
+++ kaya/ultis.py
@@ -1,5 +1,6 @@
 from djitellopy import Tello
 import cv2
+import numpy as np 
 
 def initializeTello():
     myDrone = Tello()
@@ -24,8 +25,44 @@ def findFace(img):
     faceCascade = cv2.image.png('image.png')
     imgGray =  cv2.cvtColor(img.cv2.COLOR_BGR@imgGray)  
     faces = faceCascade.detectMultiScale("imgGray 1.2,4")
+    
+    myFaceListC = []
+    myFaceListArea = []
 
     for (x,y,w,h) in faces: 
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+        cx = x + w//2 
+        cy = y + h//2 
+        area = w*h
+        myFaceListArea.append(area)
+        myFaceListC.append([cx,cy])
 
-    return img  
+    if len(myFaceListArea) !=0:
+        i = myFaceListArea.index(max(myFaceListArea))
+        return img, [myFaceListC[i],myFaceListArea[i]]
+    else: 
+        return img,[[0,0],0]
+    
+def trackFace(myDrone,info,w,pid,pError):
+
+ ## PID
+  error = info[0][0] - w//2
+  speed = pid[0]*error + pid[1]*(error-pError)
+  speed = np.clip(speed,-100,100)
+  print
+  if info[0][0] !=0:
+      myDrone.yaw_velocity = speed
+  else:
+       myDrone.for_back_velocity = 0 
+       myDrone. left_rightvelocity = 0
+       myDrone.up_down_velocity = 0 
+       myDrone.yaw_velocity = 0
+       error = 0
+       if myDrone.send_rc_control:
+          myDrone.send_rc_control(myDrone.left_right_velocity,
+                               myDrone.for_back_velocity,
+                               myDrone.up_down_velocity,
+                               myDrone.yaw_velocity)
+    
+ 
+
