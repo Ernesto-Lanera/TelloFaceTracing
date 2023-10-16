48054bd1a41a9967fedc85c1dfb787c20ea8bf94
SMDSTEAL <258288@cursist.rockopnh.nl>
2023-10-16 14:14:56 +0200

    drone webcam labview

--- face_recognition/main.py
+++ face_recognition/main.py
@@ -1,65 +1,51 @@
-import threading
-import socket
-from djitellopy import Tello
 import cv2
+import os
+import socket
 import keyboard
+from djitellopy import Tello
 
-class ClientHandler(threading.Thread):
-    def __init__(self, client_socket, tello):
-        super().__init__()
-        self.client_socket = client_socket
-        self.tello = tello
+# Create a TCP/IP socket
+sock_dashboard = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def run(self):
-        while True:
-            data = self.client_socket.recv(1024)
-            if not data:
-                print("Connection closed by client.")
-                break
+# Bind the socket to a specific address and port
+server_address = ('localhost', 12345)
+sock_dashboard.bind(server_address)
 
-            # Process client commands
-            command = data.decode()
-            if command == "takeoff":
-                self.tello.takeoff()
-            elif command == "land":
-                self.tello.land()
-            elif command == "streamon":
-                self.tello.streamon()
-            elif command == "streamoff":
-                self.tello.streamoff()
-            else:
-                print("Unknown command:", command)
+# Listen for incoming connections
+sock_dashboard.listen(1)
 
-        self.client_socket.close()
+# Initialize Tello drone
+tello = Tello()
+tello.connect()
+tello.streamon()
 
-def start_server(tello):
-# Define the host and port to listen on
-    host = "127.0.0.1"  # Use "0.0.0.0" to accept connections from any IP
-    port = 12345
+# Load pre-trained face detection model
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-    # Create a TCP socket
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Create a folder to save captured images
+if not os.path.exists("captured_images"):
+    os.mkdir("captured_images")
 
-    # Bind the socket to the specified host and port
-    server_socket.bind((host, port))
+# Tello's IP and h
+tello_address = ('192.168.10.1', 8889)
 
-    # Listen for incoming connections (maximum of 5)
-    server_socket.listen(5)
+# Create a UDP socket for sending commands
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    print(f"Server listening on {host}:{port}...")
-    
-    while True:
-        # Accept a new incoming connection
-        client_socket, client_address = server_socket.accept()
-        
-        print(f"Accepted connection from {client_address}")
+# Bind the socket to a local port to receive responses
+sock.bind(('', 9000))
 
-        # Start a new thread to handle the client
-        client_handler = ClientHandler(client_socket, tello)
-        client_handler.start()
+# Function to send a command to the Tello Drone
+def send_command(command):
+    try:
+        sock.sendto(command.encode('utf-8'), tello_address)
+        response, ip = sock.recvfrom(1024)
+        return response.decode('utf-8')
+    except Exception as e:
+        return str(e)
 
 def control_drone(tello, key):
- # Define the drone control commands based on keyboard inputs
+    # Define the drone control commands based on keyboard inputs
     if key == 'w':
         tello.send_rc_control(0, 20, 0, 0)  # Move forward
     elif key == 's':
@@ -73,94 +59,76 @@ def control_drone(tello, key):
     elif key == 'k':
         tello.send_rc_control(0, 0, -20, 0)  # Move down
     elif key == 'h':
-        tello.send_rc_control(0, 0, 0, 0) #hover
+        tello.send_rc_control(0, 0, 0, 0)  # Hover
     elif key == 'l':
         tello.send_rc_control(0, 0, 0, 20)  # Turn clockwise
     elif key == 'j':
         tello.send_rc_control(0, 0, 0, -20)  # Turn counterclockwise
     elif key == 'q':
         tello.land()  # Land
-        
+
 def main():
-    tello = Tello()  # Create a Tello object
-    tello.connect()  # Connect to the Tello drone
-    tello.streamon()  # Start the video stream
-    tello.takeoff()  # Take off
+    try:
+        # Take off
+        tello.takeoff()
 
-    # Initialize face detection
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        # Add hotkeys to control the drone
+        keyboard.add_hotkey('w', lambda: control_drone(tello, 'w'))
+        keyboard.add_hotkey('s', lambda: control_drone(tello, 's'))
+        keyboard.add_hotkey('a', lambda: control_drone(tello, 'a'))
+        keyboard.add_hotkey('d', lambda: control_drone(tello, 'd'))
+        keyboard.add_hotkey('i', lambda: control_drone(tello, 'i'))
+        keyboard.add_hotkey('k', lambda: control_drone(tello, 'k'))
+        keyboard.add_hotkey('j', lambda: control_drone(tello, 'j'))
+        keyboard.add_hotkey('l', lambda: control_drone(tello, 'l'))
+        keyboard.add_hotkey('h', lambda: control_drone(tello, 'h'))
+        keyboard.add_hotkey('q', tello.land)
 
-    # Add hotkeys to control the drone
-    keyboard.add_hotkey('w', lambda: control_drone(tello, 'w'))
-    keyboard.add_hotkey('s', lambda: control_drone(tello, 's'))
-    keyboard.add_hotkey('a', lambda: control_drone(tello, 'a'))
-    keyboard.add_hotkey('d', lambda: control_drone(tello, 'd'))
-    keyboard.add_hotkey('i', lambda: control_drone(tello, 'i'))
-    keyboard.add_hotkey('k', lambda: control_drone(tello, 'k'))
-    keyboard.add_hotkey('j', lambda: control_drone(tello, 'j'))
-    keyboard.add_hotkey('l', lambda: control_drone(tello, 'l'))
-    keyboard.add_hotkey('h', lambda: control_drone(tello, 'h'))
-    keyboard.add_hotkey('q', tello.land)
-
-    run_drone = True  # Flag to control the main loop
-
-    # Define the host and port to listen on
-    host = "127.0.0.1"  # Use "0.0.0.0" to accept connections from any IP
-    port = 12345
-
-    # Create a TCP socket
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # Bind the socket to the specified host and port
-    server_socket.bind((host, port))
-
-    # Listen for incoming connections (maximum of 5)
-    server_socket.listen(5)
-
-    print(f"Server listening on {host}:{port}...")
-
-    while run_drone:
-        # Accept a new incoming connection
-        client_socket, client_address = server_socket.accept()
-        
-        print(f"Accepted connection from {client_address}")
-
-        # Start a new thread to handle the client
-        client_handler = ClientHandler(client_socket, tello)
-        client_handler.start()
-
-        try:
-            frame = tello.get_frame_read().frame
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-            faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
-
-            for (x, y, w, h) in faces:
-                if w * h < 4000:  # Change the size threshold as needed
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)  # Red color for smaller faces
-                else:
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)  # Green color for other faces
-
-                roi_gray = gray[y:y + h, x:x + w]
-                roi_color = frame[y:y + h, x:x + w]
-
-            cv2.imshow('Tello Face Detection', frame)
-
-            # Check for keyboard input
-            if keyboard.is_pressed('w') or keyboard.is_pressed('s') or keyboard.is_pressed('a') or keyboard.is_pressed('d') or keyboard.is_pressed('q'):
-                key = keyboard.read_event().name
-                control_drone(tello, key)
-            elif keyboard.is_pressed('esc'):
-                run_drone = False
+        # wait for connection
+        connection, client_address = sock_dashboard.accept()
             
-        except KeyboardInterrupt:
-            tello.land()
-            run_drone = False
+        while True:
         
-    tello.land()
-    cv2.destroyAllWindows()
-    tello.streamoff()
-    tello.end()
+            try:
+                print('connection from', client_address)
+
+                frame = tello.get_frame_read().frame
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+                faces = face_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+                for (x, y, w, h) in faces:
+                    if w * h < 4000:  # Change the size threshold as needed
+                        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)  # Red color for smaller faces
+                    else:
+                        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)  # Green color for other faces
+
+                    roi_gray = gray[y:y + h, x:x + w]
+                    roi_color = frame[y:y + h, x:x + w]
+                    
+                print(frame.shape)
+                    
+                # wait for labview command
+                data = connection.recv(16)
+
+                # Display the frame with face detection
+                if data:
+                    connection.sendall(frame)
+                else:
+                    print('no data from', client_address)
+                    break
+            except Exception as e:
+                print(e)
+
+    except KeyboardInterrupt:
+        tello.land()
+    finally:
+        # Clean up the connection
+        connection.close()
+        tello.land()
+        tello.streamoff()
+        cv2.destroyAllWindows()
+        tello.end()
 
 if __name__ == "__main__":
     main()
